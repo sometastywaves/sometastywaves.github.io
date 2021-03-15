21fef6afc60c5acedff952def71b72ada40ae82f
Use your GitHub username <Enter your email address>
2021-03-15 15:07:06 -0700

    commit

--- lesson5/js/p5-script.js
+++ lesson5/js/p5-script.js
@@ -1,10 +1,24 @@
-function toggleMenu(){
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");}
+function toggleMenu() {
+  document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
 function Yearjs() {
   var n = new Date()
   var y = n.getFullYear();
   document.getElementById("year").innerHTML = y
 }
 Yearjs();
-const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
+
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric"
+};
 document.getElementById("timework").innerHTML = new Date().toLocaleDateString("en-us", options);
+
+var today = new Date();
+var weekday = today.getDay();
+  if (weekday == 5) {
+  document.getElementById("banner").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+}
