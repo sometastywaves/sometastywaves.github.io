b18b22228566b2827948ce33384b6b53bb295c2e
Use your GitHub username <Enter your email address>
2021-03-20 19:22:04 -0700

    commit

---
+++ lesson11/js/weather.js
@@ -0,0 +1,5 @@
+const forecastURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=cc9663bac24d48ffba1cbf202d93ed9f&units=imperial";
+fetch(forecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+      console.log(jsObject);
